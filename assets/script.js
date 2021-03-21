ec0d966b42c991965183c87f260c8009943533dd
ThomasPlunkett <thomas.plunkett.music@gmail.com>
2021-03-20 21:14:39 -0400

    tried to set if else statement. will try again. no modification

--- assets/script.js
+++ assets/script.js
@@ -15,6 +15,9 @@ function generatePassword() {
   // TODO: WRITE THE CODE THAT ASKS THE USER ABOUT THE PARAMETERS FOR THEIR PASSWORD
   var text;
   var generateBtn = prompt("How many characters would you like your password to contain:", "enter number");
+    generateBtn.addEventListener("click", function() {
+     
+  
   // TODO: USE THOSE PARAMETERS TO GENERATE A NEW PASSWORD
   
   // TODO: RETURN THAT PASSWORD
@@ -23,7 +26,6 @@ function generatePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 // code for first prompt: but remove tagname needs to listen for click generate password
 // var tagName = prompt("How many characters would you like your password to contain:", "enter number");
